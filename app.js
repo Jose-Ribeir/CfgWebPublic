49b51dcca644a7def24b18273cb01037103f8173
josep <jopirulim@gmail.com>
2022-04-24 23:09:04 +0100

    hgujj

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const bodyParser = require('body-parser')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+const users = require('./routes/user')
+const stores = require('./routes/store')
 var app = express();
 
 app.use(logger('dev'));
@@ -13,8 +14,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users1', usersRouter);
+app.get('/users', users.getUsers)
+app.get('/users/:id', users.getUserById)
+app.post('/users', users.createUser)
+app.put('/users/:id', users.updateUser)
+app.delete('/users/:id', users.deleteUser)
+
+app.get('/stores', stores.getStores)
 
 module.exports = app;
