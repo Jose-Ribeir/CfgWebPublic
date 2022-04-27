7c93ff4113c2d77cffeb16c7f5522486b26acb30
josep <jopirulim@gmail.com>
2022-04-27 08:59:35 +0100

    password encryotada e login

--- routes/user.js
+++ routes/user.js
@@ -44,11 +44,12 @@ const getLogin = (request, response) => {
 
   client.query('SELECT * FROM person WHERE person_email = $1 and person_password = $2', [users.person_email.toString() ,md5(users.person_password.toString())], (error, results) => {
     if (error) {
-      response.send("Wrong pass or email")
+      response.status(404).send("Wrong pass or email"+"" +
+          "User not found")
       throw error
 
     }
-    response.status(201).send("User found")
+    //response.status(201).send('User found')
     response.status(200).json(results.rows)
 
   })
